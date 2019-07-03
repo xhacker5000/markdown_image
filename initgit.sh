35d591421ee616ec87c041f59aa4eb238a3c5b5e
Xhacker <pengchuan@loocall.com>
2019-07-04 00:27:50 +0800

    su

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# markdown_image" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:xhacker5000/markdown_image.git
+git push -u origin master
